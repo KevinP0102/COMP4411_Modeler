b79fb2acd5dae8994879bd80a79e296ad0efc235
Kwan Yuen PUN <kypun@connect.ust.hk>
2024-10-10 13:04:36 +0800

    1.5B - Camera Twist

--- camera.cpp
+++ camera.cpp
@@ -15,6 +15,7 @@ const float kMouseRotationSensitivity		= 1.0f/90.0f;
 const float kMouseTranslationXSensitivity	= 0.03f;
 const float kMouseTranslationYSensitivity	= 0.03f;
 const float kMouseZoomSensitivity			= 0.08f;
+const float kMouseTwistSensitivity			= 0.03f;
 
 void MakeDiagonal(Mat4f &m, float k)
 {
@@ -91,10 +92,14 @@ void Camera::calculateViewingTransformParameters()
 	// grouped for (mat4 * vec3) ops instead of (mat4 * mat4) ops
 	mPosition = originXform * (azimXform * (elevXform * (dollyXform * mPosition)));
 
+	float twist = getTwist();
+	float twistCos = cos(twist);
+	float twistSin = sin(twist);
+
 	if ( fmod((double)mElevation, 2.0*M_PI) < 3*M_PI/2 && fmod((double)mElevation, 2.0*M_PI) > M_PI/2 )
-		mUpVector= Vec3f(0,-1,0);
+		mUpVector= Vec3f(twistSin,-twistCos,0);
 	else
-		mUpVector= Vec3f(0,1,0);
+		mUpVector= Vec3f(twistSin,twistCos,0);
 
 	mDirtyTransform = false;
 }
@@ -159,7 +164,12 @@ void Camera::dragMouse( int x, int y )
 			break;
 		}
 	case kActionTwist:
-		// Not implemented
+		{
+		setTwist(getTwist() - mouseDelta[0] * kMouseRotationSensitivity);
+
+		break;
+		}
+
 	default:
 		break;
 	}
@@ -215,6 +225,15 @@ void Camera::applyViewingTransform() {
 	// mLookAt, and twist the camera such that mUpVector is up
 	lookAt(mPosition, mLookAt, mUpVector);
 	
+}
+
+void Camera::frameAll() {
+	mDolly = -20.0f;
+	mElevation = 0.2f;
+	mAzimuth = (float)M_PI;
+	mLookAt = Vec3f(0, 0, 0);
+	mPosition = Vec3f(10, 10, 10);
+	mDirtyTransform = true;
 }
 
 #pragma warning(pop)
